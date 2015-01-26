9f137f0345b6f21e9a7d15a642a725f1b35a2dd6
Konrad Lisiecki <knrd.lisiecki@gmail.com>
2015-01-26 16:31:30 +0100

    all works, add only conditions

--- kl291649.asm
+++ kl291649.asm
@@ -1,8 +1,8 @@
 DEFAULT REL
 section .bss
-    _s:     resw    2
-    _w:     resw    2 
-    _T:     resw    4 
+    _s:     resb    4
+    _w:     resb    4 
+    _T:     resb    8 
    
 
 section .text
@@ -31,15 +31,13 @@ start2:
     ret
     
 run2:
-    push rbp
-    mov rbp, rsp
-    sub rsp, 64
+    enter 0,0
+    sub rsp, 100
 
     mov dword [rbp-4], edi      ; przenies z edi parametr do rbp - 4
 
     mov rax, [_T]               ; na rax daj wartosc pod adresem _T
     mov qword [rbp-48], rax     ; przypisz na rbp-48 to co w rax, czyli _T 
-   
 
     mov rax, [_w]               ; przypisz do rax to co w _w
     shl rax, 3                  ; przesun bitowo w lewo o 3, pomnoz razy 8
@@ -67,20 +65,14 @@ MALLOC_LOOP:
     mov eax, [rbp-8]
     add eax, 1
     mov dword [rbp-8], eax
-    jmp MALLOC_LOOP
+    jmp MALLOC_LOOP 
 
+AFTER_MALLOC_LOOP:               ; kod ktory jest po petli z alokacja 
 
-AFTER_MALLOC_LOOP:               ; kod ktory jest po petli z alokacja
-    mov rax, qword [rbp-24]     ; przenies do rejestru rax zmienna cT
-    mov qword [rbp-32], rax     ; przypisz na zmienna fT rejestr rax
+    mov rax, qword [rbp-24]      ; przenies do rejestru rax zmienna cT
+    mov qword [rbp-32], rax      ; przypisz na zmienna fT rejestr rax
 
     mov dword [rbp-8], 0
-
-    ; mov rsi, 0
-    ; mov rdi, 0
-    ; mov r8, qword [rbp-48]
-    ; mov rdi, [r8+rdi*8]
-    ; mov [rdi+rsi], byte 88
 
 FIRST_LOOP:
     mov eax, dword [rbp-8]
@@ -103,58 +95,48 @@ THIRD_LOOP:
 ;                    w srodku petli - sprawdzenie ifow
 ; ===============================================================================
 
-    mov dword [rbp-52], 0         ; przypisanie na l 0
-
-    cmp dword [rbp-12], 0
-    jle AFTER_FIRST_IF
-
-    mov eax, dword [rbp-12]
-    sub eax, 1
-    movsxd rcx, eax 
-    movsxd rdx, dword [rbp-16]
-    mov rsi, qword [rbp-48]
-    mov rdx, [rsi+rdx*8]
-    mov eax, [rdx+rcx]              ; byc moze trezeba bedzie inna instrukcje zastosowac
-    cmp eax, 42                     ; porownaj to co jest w cT2[y][x-1] z tym *
-    jne AFTER_FIRST_IF              ; jesli nie jest rowna
-
-    mov eax, dword [rbp-52]
-    add eax, 1
-    mov dword [rbp-52], eax 
-
-
-
 AFTER_FIRST_IF:
     jmp STAR_IF
 
 ; ===============================================================================
 ;                    w srodku petli - aktualizacja pol gry w zycie
 ; ===============================================================================
-    
+   
 
+STAR_IF: 
 
-
-
-
-STAR_IF:
     movsxd rax, dword [rbp-12]
     movsxd rcx, dword [rbp-16]
     mov rdx, qword [rbp-48]
     mov rcx, [rdx+rcx*8]
-    mov esi, [rcx+rax]
-    cmp esi, 0
-    jne AFTER_STAR_IF
+    movzx rsi, byte [rcx+rax]
+    mov rdx, 42
 
-
-
+    cmp rsi, rdx
+    jne UNDERSCORE_IF
+ 
     movsxd rsi, dword [rbp-12]
-    movsxd rdi, dword [rbp-16]
-    mov r8, qword [rbp-24]
-    mov rdi, [r8+rdi*8]
-    mov [rdi+rsi], byte 42  
+    movsxd rdi, dword [rbp-16] 
+    mov rax, qword [rbp-24]
+    mov rdi, [rax+rdi*8] 
+    mov [rdi+rsi], byte 42
 
+UNDERSCORE_IF:
+    movsxd rax, dword [rbp-12]
+    movsxd rcx, dword [rbp-16]
+    mov rdx, qword [rbp-48]
+    mov rcx, [rdx+rcx*8]
+    movzx rsi, byte [rcx+rax]
+    mov rdx, 95
 
-
+    cmp rsi, rdx
+    jne AFTER_STAR_IF
+ 
+    movsxd rsi, dword [rbp-12]
+    movsxd rdi, dword [rbp-16] 
+    mov rax, qword [rbp-24]
+    mov rdi, [rax+rdi*8] 
+    mov [rdi+rsi], byte 95
 
 
 AFTER_STAR_IF:
@@ -176,31 +158,50 @@ SECOND_LOOP_FINISH:
     jmp SECOND_LOOP
 
 AFTER_SECOND_LOOP:
-    jmp FIRST_LOOP_FINISH
+    jmp MEMCPY
 
-FIRST_LOOP_FINISH:
+MEMCPY:
 
 
 ; ===============================================================================
 ;                    podmieniam tablice na koncu ostatniej petli
 ; ===============================================================================
 
-    mov rax, qword [rbp-48]
-    mov qword [rbp-40], rax
-    mov rax, qword [rbp-24]
-    mov qword [rbp-48], rax
-    mov rax, qword [rbp-40]
-    mov qword [rbp-24], rax
+    mov dword [rbp-100], 0
+MEMCPY_LOOP:
+    mov eax, [rbp-100]
+    cmp eax, [_w]
+    jge FIRST_LOOP_FINISH 
+
+    movsxd rax, dword [rbp-100]
+    mov rdx, qword [rbp-48]
+    mov rdi, [rdx+rax*8]
+    movsxd rax, dword [rbp-100]
+    mov rdx, qword [rbp-24]
+    mov rsi, [rdx+rax*8]
+    movsxd rdx, [_s] 
+    extern memcpy
+    call memcpy
+
+    mov eax, dword [rbp-100]
+    add eax, 1
+    mov dword [rbp-100], eax
+    jmp MEMCPY_LOOP
+
+FIRST_LOOP_FINISH:
 
     mov eax, dword [rbp-8]
     add eax, 1
     mov dword [rbp-8], eax
     jmp FIRST_LOOP    
 
-AFTER_FIRST_LOOP:
 
-    add rsp, 64
-    pop rbp
+; ===============================================================================
+;                    protokol koncowy
+; ===============================================================================
+
+AFTER_FIRST_LOOP: 
+    leave
     ret
     
     
