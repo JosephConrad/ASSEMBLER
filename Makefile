3045d840f016999bb9a1de37378560a284a442f1
Konrad Lisiecki <knrd.lisiecki@gmail.com>
2015-02-13 04:45:05 +0100

    Ass: ready

--- Makefile
+++ Makefile
@@ -1,4 +1,3 @@
 all: 
 	nasm -g -f elf64 kl291649.asm
 	gcc -o test -Wall -g kl291649.o test.c
-	./test
--- test.c
+++ test.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_SIZE 1024
+
 //#include "zad1.c"
 extern void start(int szer, int wys, unsigned char ** T);
-extern int run(int ile_krokow);
+extern void run(int ile_krokow);
 
 void printTable(int s, int w, unsigned char ** T) {
 	int i, j;
@@ -16,104 +18,54 @@ void printTable(int s, int w, unsigned char ** T) {
 	printf("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
 }
 
-int main () {
+int main (int argc, char * argv[]) {
 
-	int szer = 100;
-	int wys = 100;
-	unsigned char ** T;
+    printf("\nFirst assignment - Assebler\n\n");
 
+    if ( argc != 2 ) { 
+        printf( "usage: %s filename", argv[0] );
+        return 0;
+    }
+    FILE *file = fopen(argv[1], "r");
+	
+    if ( file == 0 ) {
+        printf( "Could not open file\n" );
+        exit(EXIT_FAILURE);
+    }
+    
+	int wys, szer, obroty, step;
+    
+    fscanf(file, "%d", &wys); 
+    fscanf(file, "%d", &szer);
+    fscanf(file, "%d", &obroty);  
+    fscanf(file, "%d", &step); 
+    
+    
+    unsigned char ** T;
 	int k, i, j;
 
 	T = malloc(sizeof(unsigned char *) * wys);
 	for (i = 0; i < wys; ++i) {
 		T[i] = malloc(sizeof(unsigned char) * szer);
-	}
+	} 
 
-
-	printf("\n\nTest nr 1\n\n");
-
-
-	for (i = 0; i < wys; ++i) {
-		for (j = 0; j < szer; ++j){
-			T[i][j] = '_';
-		} 
-	}
-
-	for (i = 0; i < wys; ++i) {
-		for (j = 0; j < szer; ++j){
-			if ((i+j) % 2 == 0)
-				T[i][j] = '*';
-		} 
-	}
-	start(szer, wys, (unsigned char **)T);
+	printf("\n\n%s\n\n", argv[0]);
+	char buf;
  
+ 	for (i = 0; i < wys; i++) {
+ 		for (j = 0; j < szer; ++j){
+ 			fscanf(file, "%s", &T[i][j]); 
+		}
+		fgetc(file);
+ 	}
 
+	start(szer, wys, (unsigned char **)T);
 	printTable(szer, wys, T);
 
-	for (k = 0; k < 15; ++k) {
-		int a= run(1);
-
-		printf("%d\n", a);
+	for (k = 0; k < obroty; ++k) {
+		run(step); 
 		printTable(szer, wys, T);
  		
 	}
-
-	printf("\n\nTest nr 2\n\n");
-	szer = 20;
-	wys = 20;
-
-	T = malloc(sizeof(unsigned char *) * wys);
-	for (i = 0; i < wys; ++i) {
-		T[i] = malloc(sizeof(unsigned char) * szer);
-	}
-
-	for (i = 0; i < wys; ++i) {
-		for (j = 0; j < szer; ++j){
-			T[i][j] = '_';
-		} 
-	}
-
-	for (i = 0; i < szer; i++) {
-		T[0][i] = '*'; 
-		T[2][i] = '*'; 
-		T[4][i] = '*'; 
-	}
-
-
-	start(szer, wys, (unsigned char **)T);
-
- 	//printTable(szer, wys, T);
-
-	for (k = 0; k < 3; ++k) {
-		int a= run(1);
-
-		printf("%d\n", a);
- 		//printTable(szer, wys, T);
-	}
-
-	// printf("\n\nTest nr 3\n\n");
-
-	// for (i = 0; i < wys; ++i) {
-	// 	for (j = 0; j < szer; ++j){
-	// 		T[i][j] = '_';
-	// 	} 
-	// }
-
-	// for (i = 0; i < wys; ++i) {
-	// 	for (j = 0; j < szer; ++j){
-	// 		if ((i+j) % 2 == 0)
-	// 			T[i][j] = '*';
-	// 	} 
-	// }
-
-	// start(szer, wys, (unsigned char **)T);
-
- // 	printTable(szer, wys, T);
-
-	// for (k = 0; k < 6; ++k) {
-	// 	run(1);
- // 		printTable(szer, wys, T);
-	// }
-
 	return 0;
 }
